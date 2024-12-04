68e768b10c8bf81eb2bd86c02b3feb70cfd3c7e5
Ridwan Ibidunni <aljebraschoolofthought@gmail.com>
2024-12-04 12:41:50 +0100

    Keep only contents of llm_streamlit_deploy folder

--- fastapi_complete_code/schemas/files.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class FileResponse(BaseModel):
-    filename : str
-    content_type : str
-    file_size : int
-
-class MultipleFileResponse(BaseModel):
-    files : list[FileResponse]
--- fastapi_complete_code/schemas/product.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class ProductOut(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: Optional[list[str]] = []
